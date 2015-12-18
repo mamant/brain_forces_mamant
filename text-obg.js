49dae281957a125902fb6fd07da9b0489a7a4da4
Anton Mamchenko <anton.mamant@gmail.com>
2015-12-18 20:46:32 +0200

    deposit calk part 1

---
+++ text-obg.js
@@ -0,0 +1,52 @@
+
+var menu = {
+	0: {
+		icon: 'icn-menu-home',
+		name: 'Home'
+	},
+	1: {
+		icon: 'icn-menu-dashboard',
+		name: 'Dashboard',
+		submenu: {
+			0: {
+				...
+			}
+		}
+	}
+}
+
+vat timeline = {
+	0: {
+		date: '21.12.15',
+		status: 'sport',
+		header: 'Bla bla',
+		text: 'bla bla bla ....'
+	},
+	1: {
+		date: '21.12.15',
+		status: 'sport',
+		header: 'Blax bla',
+		text: 'bla bla bla ....'
+	}
+}
+
+var
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
